eb09681aa7b9ccbe1515d216ce6eb6fbf2559825
topepols <kristophermacarubbo@gmail.com>
2025-11-25 23:39:00 +0800

    update

--- out/electron-inventory-app-win32-x64/resources/app/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  exportPDF: () => ipcRenderer.invoke('export-pdf'),
-  exportCSV: (csv) => ipcRenderer.invoke('export-csv', csv)
-});
