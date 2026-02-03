643d9bb2c99ddc939dc8323ab5c95303a96a4728
topepols <kristophermacarubbo@gmail.com>
2026-02-03 08:59:57 +0800

    up

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, 'jdg.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
